12d9573432cabe48993f9f95b5eb0aab04643a5a
ows <ows1539@gmail.com>
2024-03-25 23:53:06 +0900

    commit p2-1.c

--- p2-1.c
+++ p2-1.c
@@ -41,7 +41,7 @@
     printf("The sum is: %f\n\n", answer); //sum3 함수의 반환값 출력
 }
 
- float sum1(float list[], int n) //sum1은 매개변수를 list[]와 int n 형태로 받고있음
+ float sum1(float list[], int n) //sum1은 매개변수를 list[]로 배열의 주소값을 받고, int n을 받고있음
  {
     printf("list \t= %p\n", list); //list의 메모리 주소를 출력
     printf("&list \t= %p\n\n", &list); //list의 메모리 주소를 출력
@@ -54,7 +54,7 @@
     return tempsum; //tempsum 반환
  }
 
- float sum2(float *list, int n) //sum2는 매개변수를 *list와 int n 형태로 받고있음
+ float sum2(float *list, int n) //sum2는 매개변수를 *list로 배열의 주소값을 받고, int n을 받고있음
  {
     printf("list \t= %p\n", list);  //list의 메모리 주소를 출력
     printf("&list \t= %p\n\n", &list);  //list의 메모리 주소를 출력
@@ -68,7 +68,12 @@
  }
 
  /* stack variable reuse test */
- float sum3(int n, float *list) //sum3는 매개변수를 int n과 *list로 받고있음
+ /*sum3함수는 sum1과 sum2와 달리 int n을 먼저 받고 *list를 받는데 c에서 함수는 매개변수를 호출할 때
+  거꾸로 스택에 쌓이게 되므로 마지막 매개변수가 먼저 스택에 쌓이고 첫 번째 매개변수가 마지막 스택에 
+  쌓이게 되어 sum3에서는 list가 n 다음에 위치하게 되고 int의 크기가 4byte이므로
+  sum1 sum2보다 4byte큰 주소값이 출력되었다.*/
+  
+ float sum3(int n, float *list) // sum3은 매개변수를 *list로 배열의 주소값을 받고, int n을 받고있음 
  {
     printf("list \t= %p\n", list); //list의 메모리 주소를 출력
     printf("&list \t= %p\n\n", &list); //list의 메모리 주소를 출력
